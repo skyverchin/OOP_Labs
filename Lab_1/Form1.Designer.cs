60c37a87802c8c1f37da75fe6bf511101edfa8b5
Ō̴n̴͂l̸̋i̶͒n̴̏e̷̓_̴̀À̷ď̷m̷̕ǐ̷ǹ̵ <92204862+skyverchin@users.noreply.github.com>
2022-10-07 09:44:14 +0300

    Add files via upload

---
+++ Lab_1/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Lab_1
+{
+    partial class TaskTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.InputA = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.InputB = new System.Windows.Forms.TextBox();
+            this.InputC = new System.Windows.Forms.TextBox();
+            this.SubmitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Input A";
+            // 
+            // InputA
+            // 
+            this.InputA.Location = new System.Drawing.Point(106, 40);
+            this.InputA.Name = "InputA";
+            this.InputA.Size = new System.Drawing.Size(100, 22);
+            this.InputA.TabIndex = 2;
+            this.InputA.TextChanged += new System.EventHandler(this.InputA_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Input B";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Input C";
+            // 
+            // InputB
+            // 
+            this.InputB.Location = new System.Drawing.Point(106, 79);
+            this.InputB.Name = "InputB";
+            this.InputB.Size = new System.Drawing.Size(100, 22);
+            this.InputB.TabIndex = 5;
+            this.InputB.TextChanged += new System.EventHandler(this.InputB_TextChanged);
+            // 
+            // InputC
+            // 
+            this.InputC.Location = new System.Drawing.Point(106, 121);
+            this.InputC.Name = "InputC";
+            this.InputC.Size = new System.Drawing.Size(100, 22);
+            this.InputC.TabIndex = 6;
+            this.InputC.TextChanged += new System.EventHandler(this.InputC_TextChanged);
+            // 
+            // SubmitButton
+            // 
+            this.SubmitButton.Location = new System.Drawing.Point(91, 173);
+            this.SubmitButton.Name = "SubmitButton";
+            this.SubmitButton.Size = new System.Drawing.Size(75, 23);
+            this.SubmitButton.TabIndex = 7;
+            this.SubmitButton.Text = "Submit";
+            this.SubmitButton.UseVisualStyleBackColor = true;
+            this.SubmitButton.Click += new System.EventHandler(this.SubmitButton_Click_1);
+            // 
+            // TaskTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(264, 250);
+            this.Controls.Add(this.SubmitButton);
+            this.Controls.Add(this.InputC);
+            this.Controls.Add(this.InputB);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.InputA);
+            this.Controls.Add(this.label1);
+            this.Name = "TaskTest";
+            this.Text = "TaskTest";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox InputA;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox InputB;
+        private System.Windows.Forms.TextBox InputC;
+        private System.Windows.Forms.Button SubmitButton;
+    }
+}
+
---
+++ Lab_1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_1
+{
+    public partial class TaskTest : Form
+    {
+        public TaskTest()
+        {
+            InitializeComponent();
+        }
+        private void InputA_TextChanged(object sender, EventArgs e){}
+        private void InputB_TextChanged(object sender, EventArgs e){}
+        private void InputC_TextChanged(object sender, EventArgs e){}
+
+        private void SubmitButton_Click_1(object sender, EventArgs e)
+        {
+            double a = 0, b = 0, c = 0, res = 0;
+
+            a = double.Parse(InputA.Text);
+            b = double.Parse(InputB.Text);
+            c = double.Parse(InputC.Text);
+
+            if (a <= 0 || b <= 0 || c <= 0)
+            {
+                MessageBox.Show("U are not UwU");
+            }
+            else
+            {
+                res = 0.5 * Math.Sqrt(4 * a * a - c * c);
+                MessageBox.Show("Result = " + res);
+            }
+        }
+    }
+}
