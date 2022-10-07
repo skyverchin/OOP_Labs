6b2a3e753c603003f39cfe564820ef9b70f2a9ee
Ō̴n̴͂l̸̋i̶͒n̴̏e̷̓_̴̀À̷ď̷m̷̕ǐ̷ǹ̵ <92204862+skyverchin@users.noreply.github.com>
2022-10-07 10:03:12 +0300

    Add files via upload
    .net 3.1

---
+++ Lab_2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab_2
+{
+    internal class Program
+    {
+        class Array
+        {
+            int x;
+
+            public int X
+            {
+                get { return x; }
+                set { x = value; }
+            }
+        }
+
+        class MainClass
+        {
+            static void Main(string[] args)
+            {
+
+                Array[] MyArray = new Array[5];
+
+                for (int i = 0; i < 5; i++)
+                {
+                    MyArray[i] = new Array();
+                    MyArray[i].X = 2+i;
+                    Console.WriteLine("Firstly generated array " + MyArray[i].X);
+                }
+                int MinimalNumber = MyArray.Min(p => p.X);
+                Console.WriteLine("Most less number in array " + MinimalNumber);
+            }
+        }
+    }
+}
