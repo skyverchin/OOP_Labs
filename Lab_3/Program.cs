05e0fd470e11cb5d7bdb51e34e98ee98ab08a822
Ō̴n̴͂l̸̋i̶͒n̴̏e̷̓_̴̀À̷ď̷m̷̕ǐ̷ǹ̵ <92204862+skyverchin@users.noreply.github.com>
2022-10-07 10:43:19 +0300

    Add files via upload
    .net 3.1

---
+++ Lab_3/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+
+namespace Lab_3
+{
+    internal class Program
+    {
+        class Array
+        {
+            int x;
+            
+            public int X
+            {
+                get { return x; }
+                set { x = value; }
+            }
+
+            Array[] MyArray = new Array[5];
+            Array[] MyArraySecond = new Array[5];
+
+            public void ArrayFill()
+            {
+                Console.WriteLine("Generated first array");
+                for (int i = 0; i < 5; i++)
+                {
+                    MyArray[i] = new Array();
+                    MyArray[i].X = 2+i;
+                    Console.WriteLine(MyArray[i].X);
+                }
+
+                Console.WriteLine("Generated second array");
+                for (int i = 0; i < 5; i++)
+                {
+                    MyArraySecond[i] = new Array();
+                    MyArraySecond[i].X = 3+i;
+                    Console.WriteLine(MyArraySecond[i].X);
+                }
+            }
+
+            public void ArrayChose()
+            {
+
+                int MaxNumber = MyArray.Max(p => p.X);
+                Console.WriteLine("Most maximum number in first array " + MaxNumber);
+
+                int MaxNumber2 = MyArraySecond.Max(p => p.X);
+                Console.WriteLine("Most maximum number in second array " + MaxNumber2);
+
+                if (MaxNumber < MaxNumber2)
+                {
+                    Console.WriteLine("Second Array have biggest less number" + MaxNumber2);
+                }
+                else
+                {
+                    Console.WriteLine("First Array have biggest less number" + MaxNumber);
+                }
+            }
+        }
+        class MainClass
+        {
+            static void Main(string[] args)
+            {
+                Array Array = new Array();
+                Array.ArrayFill();
+                Array.ArrayChose();
+            }
+        }
+    }
+}
