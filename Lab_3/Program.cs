a7e7732d3ba8beaa1cc74431fe39eed790627cc7
Ō̴n̴͂l̸̋i̶͒n̴̏e̷̓_̴̀À̷ď̷m̷̕ǐ̷ǹ̵ <92204862+skyverchin@users.noreply.github.com>
2022-10-19 13:09:42 +0300

    Add files via upload

--- Lab_3/Program.cs
+++ Lab_3/Program.cs
@@ -7,42 +7,15 @@ namespace Lab_3
     {
         class Array
         {
-            int x;
-            
-            public int X
-            {
-                get { return x; }
-                set { x = value; }
-            }
-
-            public Array[] MyArray = new Array[5];
-            public Array[] MyArraySecond = new Array[5];
-
-            /* public void ArrayFill()
-             {
-                 Console.WriteLine("Generated first array");
-                 for (int i = 0; i < 5; i++)
-                 {
-                     MyArray[i] = new Array();
-                     MyArray[i].X = 2+i;
-                     Console.WriteLine(MyArray[i].X);
-                 }
-
-                 Console.WriteLine("Generated second array");
-                 for (int i = 0; i < 5; i++)
-                 {
-                     MyArraySecond[i] = new Array();
-                     MyArraySecond[i].X = 3+i;
-                     Console.WriteLine(MyArraySecond[i].X);
-                 }
-             }*/
+            public int[] MyArray;
+            public int[] MyArraySecond;
 
             public void ArrayChose()
             {
-                int MaxNumber = MyArray.Max(p => p.X);
+                int MaxNumber = MyArray.Max(p => p);
                 Console.WriteLine("Most maximum number in first array " + MaxNumber);
 
-                int MaxNumber2 = MyArraySecond.Max(p => p.X);
+                int MaxNumber2 = MyArraySecond.Max(p => p);
                 Console.WriteLine("Most maximum number in second array " + MaxNumber2);
 
                 if (MaxNumber < MaxNumber2)
@@ -62,21 +35,8 @@ namespace Lab_3
                 Array Array = new Array();
                 //Array.ArrayFill();
 
-                Console.WriteLine("Generated first array");
-                for (int i = 0; i < 5; i++)
-                {
-                    Array.MyArray[i] = new Array();
-                    Array.MyArray[i].X = 2+i;
-                    Console.WriteLine(Array.MyArray[i].X);
-                }
-
-                Console.WriteLine("Generated second array");
-                for (int i = 0; i < 5; i++)
-                {
-                    Array.MyArraySecond[i] = new Array();
-                    Array.MyArraySecond[i].X = 3+i;
-                    Console.WriteLine(Array.MyArraySecond[i].X);
-                }
+                Array.MyArray = new int[5] { 1, 2, 3, 4, 5};
+                Array.MyArraySecond = new int[5] { 1, 2, 3, 4, 8};
 
                 Array.ArrayChose();
             }
---
+++ lab_4/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Linq;
+using System.Drawing;
+using System.Drawing.Design;
+using System.Diagnostics;
+
+namespace Lab_4
+{
+    internal class Program
+    {
+        public class XYZ
+        {
+            public int x, y; //Decart
+            public int r, f; //Polar
+            public int p, fi; //Polar
+            public double TmpR, TmpF, TmpP, TmpFi; //Temporary variables
+
+            Random rnd = new Random(); //Random generator
+
+            public void TransformPolarIntoDecart()
+            {
+                double TmpX = r*Math.Cos(f);
+                double TmpY = r*Math.Sin(f);
+                Console.WriteLine("Polar to Decart result " + "x = " + TmpX + " " + "y = " + TmpY);
+            }
+
+            public void TransformDecartIntoPolar()
+            {
+                TmpR = Math.Sqrt(Math.Pow(x, 2)+Math.Pow(y, 2));
+
+                if (TmpR == 0)
+                    TmpF = rnd.Next();
+                else
+                {
+                    if (x>0 && y>=0)
+                        TmpF = Math.Atan(y/x);
+                    else if (x>0 && y<0)
+                        TmpF = Math.Atan(y/x)+2*Math.PI;
+                    else if (x<0)
+                        TmpF = Math.Atan(y/x)+Math.PI;
+                    else if (x==0 && y>0)
+                        TmpF = Math.PI/2;
+                    else if (x==0 && y<0)
+                        TmpF = 3* Math.PI/2;
+                }
+                Console.WriteLine("Decart to Cyliner result r = " + TmpR + " fi = " + TmpF);  
+            }
+
+            public void TransformCylinderIntoPolar()
+            {
+                double TmpX = p*Math.Cos(fi);
+                double TmpY = p*Math.Sin(fi);
+
+                Console.WriteLine("Cylinder to Decart result " + "x = " + TmpX + " " + "y = " + TmpY);
+            }
+
+            public void TransformPolarIntoCylinder()
+            {
+                TmpP = Math.Sqrt(Math.Pow(x, 2)+Math.Pow(y, 2));
+
+                if (x==0 && y==0)
+                    TmpFi = 0;
+                if (x>=0)
+                    TmpFi = Math.Asin(y/TmpP);
+                if (x>0)
+                    TmpFi = Math.Atan(y/x);
+                if (x<0)
+                    TmpFi = -Math.Asin(y/TmpP)+Math.PI;
+
+                Console.WriteLine("Decart to Polar result p = " + TmpP + " fi = " + TmpFi);
+            }
+
+        }
+        class MainClass{
+            static void Main(string[] args)
+            {
+                XYZ XYZ = new XYZ();
+                XYZ.x = 1;
+                XYZ.y = -2;
+                XYZ.r = 4;
+                XYZ.f = 30;
+                XYZ.p = 3;
+                XYZ.fi = 60;
+
+                XYZ.TransformPolarIntoDecart();
+                XYZ.TransformDecartIntoPolar();
+                XYZ.TransformCylinderIntoPolar();
+                XYZ.TransformPolarIntoCylinder();
+            }
+        }
+    }
+}
