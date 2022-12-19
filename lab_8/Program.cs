f6d0ae2af1442997a38e0e28b0cfe3d27bdabc7e
Ō̴n̴͂l̸̋i̶͒n̴̏e̷̓_̴̀À̷ď̷m̷̕ǐ̷ǹ̵ <92204862+skyverchin@users.noreply.github.com>
2022-12-19 17:38:22 +0200

    Add files via upload

---
+++ lab_8/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace lab_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string compare = "Amogus";
+            Regex regex = new Regex(@"[\d]");
+            if (regex.IsMatch(compare))
+                Console.WriteLine("Number in row");
+            else
+                Console.WriteLine("OnlyText");
+        }
+    }
+}
